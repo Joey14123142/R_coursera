312ab790e58f033f39028cb2946f7f3b3ac80050
Joey14123142 <14123142d@connect.polyu.hk>
2015-10-20 13:06:00 +0800

    Assign1

--- RProgrammingAssign1.R
+++ RProgrammingAssign1.R
@@ -20,3 +20,48 @@ pollutantmean <- function(directory, pollutant, id = 1:332) {
   total <- do.call(rbind.data.frame, dt)
   mean(total[,pollutant], na.rm = T)
 }
+
+complete <- function(directory, id = 1:332) {
+  ## 'directory' is a character vector of length 1 indicating
+  ## the location of the CSV files
+
+  ## 'id' is an integer vector indicating the monitor ID numbers
+  ## to be used
+  
+  ## Return a data frame of the form:
+  ## id nobs
+  ## 1  117
+  ## 2  1041
+  ## ...
+  ## where 'id' is the monitor ID number and 'nobs' is the
+  ## number of complete cases
+  nobs <- function(id) {
+    path <- file.path(directory, paste(sprintf("%03d", as.numeric(id)), ".csv", sep=""))
+    return (sum(complete.cases(read.csv(path))))
+  }
+  return (data.frame(id=id, nobs=sapply(id, nobs)))
+}
+
+corr <- function(directory, threshold = 0) {
+  ## 'directory' is a character vector of length 1 indicating
+  ## the location of the CSV files
+  files <- list.files(directory)
+  result <- c()
+  for (file in files) {
+    dt <- read.csv(file.path(directory, file))
+    nob <- sum(complete.cases(dt))
+    if (nob > threshold){
+      result <- c(result,cor(dt$sulfate, dt$nitrate, use = "complete.obs"))
+    }
+  }
+  
+  ## 'threshold' is a numeric vector of length 1 indicating the
+  ## number of completely observed observations (on all
+  ## variables) required to compute the correlation between
+  ## nitrate and sulfate; the default is 0
+  
+  ## Return a numeric vector of correlations
+  ## NOTE: Do not round the result!
+  return (result)
+}
+
